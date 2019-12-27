c10cafadc9d8948cadc8b64a156733a8d28a6ad1
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2019-12-27 21:40:51 +0100

    Install MvvmLight nugets with libs, and create first ViewModel

---
+++ LaTeXSierotkaRemover/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaTeXSierotkaRemover.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Commands
+        public MainViewModel()
+        { }
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+    }
+}
