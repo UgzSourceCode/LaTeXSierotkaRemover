e4c4d8885f4e4b63f8721c540d8f4fadfc0a3178
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2019-12-28 00:14:38 +0100

    Move other logic to ViewModel.

--- LaTeXSierotkaRemover/MainWindow.xaml.cs
+++ LaTeXSierotkaRemover/MainWindow.xaml.cs
@@ -21,65 +21,9 @@ namespace LaTeXSierotkaRemover
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static readonly string[] prefixes = { "~", " ", Environment.NewLine, "\r\n" };
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ReadFileButton_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void DoThis_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                LoadingLockGrid.Visibility = Visibility.Visible;
-                string document = File.ReadAllText(UrlLatexTextBox.Text);
-                foreach(var sierotka in SierotkiView.Items)
-                {
-                    document = this.ReplaceDocument(document, sierotka.ToString().Trim());// document.Replace(" " + sierotka + " ", " " + sierotka + "~");
-                }
-                File.WriteAllText(UrlLatexTextBox.Text, document);
-            }
-            finally
-            {
-                LoadingLockGrid.Visibility = Visibility.Collapsed;
-            }
-
-        }
-
-        private string ReplaceDocument(string document, string sierotka)
-        {
-            string result = document;
-            foreach(string prefix in prefixes)
-            {
-                foreach(string variantOfSierotka in GetVariantsSierotka(sierotka))
-                {
-                    result = result.Replace(prefix + variantOfSierotka + " ", prefix + variantOfSierotka + "~");
-                }
-            }
-
-            return result;
-        }
-
-        private List<string> GetVariantsSierotka(string sierotka)
-        {
-            List<string> result = new List<string>();
-            if (sierotka.Length >= 2)
-            {
-                result.Add(char.ToUpper(sierotka[0]) + sierotka.Substring(1));
-            }
-            result.Add(sierotka);
-            result.Add(sierotka.ToUpper());
-            result.Add(sierotka.ToLower());
-
-            result = result.Distinct().ToList();
-
-            return result;
         }
     }
 }
--- LaTeXSierotkaRemover/ViewModels/MainViewModel.cs
+++ LaTeXSierotkaRemover/ViewModels/MainViewModel.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 
 namespace LaTeXSierotkaRemover.ViewModels
@@ -12,7 +14,7 @@ namespace LaTeXSierotkaRemover.ViewModels
     public class MainViewModel : ViewModelBase
     {
         #region Fields
-
+        private static readonly string[] prefixes = { "~", " ", Environment.NewLine, "\r\n" };
         #endregion
 
         #region Properties
@@ -29,6 +31,14 @@ namespace LaTeXSierotkaRemover.ViewModels
             get { return _sierotkiBaseAddress; }
             set { Set(ref _sierotkiBaseAddress, value); }
         }
+
+        private string _documentAddress;
+        public string DocumentAddress
+        {
+            get { return _documentAddress; }
+            set { Set(ref _documentAddress, value); }
+        }
+
 
         private bool _isVisibleLoadingLockGrid;
         public bool IsVisibleLoadingLockGrid
@@ -77,6 +87,7 @@ namespace LaTeXSierotkaRemover.ViewModels
         private void InitializeVariables()
         {
             this.SierotkiBaseAddress = "./base.txt";
+            this.DocumentAddress = "./index.tex";
         }
 
         private void LoadSierotkiBase()
@@ -95,7 +106,54 @@ namespace LaTeXSierotkaRemover.ViewModels
 
         private void RemoveSierotki()
         {
+            try
+            {
+                this.IsVisibleLoadingLockGrid = true;
+                string document = File.ReadAllText(this.DocumentAddress);
+                foreach (var sierotka in SierotkiList)
+                {
+                    document = this.ReplaceDocument(document, sierotka.ToString().Trim());
+                }
+                File.WriteAllText(this.DocumentAddress, document);
+            }
+            catch (FileNotFoundException FNFex)
+            {
+                MessageBox.Show(FNFex.Message, "File Not Found");
+            }
+            finally
+            {
+                this.IsVisibleLoadingLockGrid = false;
+            }
+        }
 
+        private string ReplaceDocument(string document, string sierotka)
+        {
+            string result = document;
+            foreach (string prefix in prefixes)
+            {
+                foreach (string variantOfSierotka in GetVariantsSierotka(sierotka))
+                {
+                    result = result.Replace(prefix + variantOfSierotka + " ", prefix + variantOfSierotka + "~");
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> GetVariantsSierotka(string sierotka)
+        {
+            List<string> result = new List<string>();
+            if (sierotka.Length >= 2)
+            {
+                result.Add(char.ToUpper(sierotka[0]) + sierotka.Substring(1));
+            }
+            result.Add(sierotka);
+            result.Add(sierotka.ToUpper());
+            result.Add(sierotka.ToLower());
+
+            result = result.Distinct().ToList();
+
+            return result;
         }
         #endregion
     }
