f6dcc291e628ea0f954444cce8c99e2fdf2fdc0c
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2019-12-28 00:04:30 +0100

    Added BooleanToVisibilityConverter and first use them.

---
+++ LaTeXSierotkaRemover/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LaTeXSierotkaRemover.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility result = Visibility.Collapsed;
+
+            if (value != null && value is bool)
+            {
+                bool input = (bool)value;
+
+                result = (input) ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
